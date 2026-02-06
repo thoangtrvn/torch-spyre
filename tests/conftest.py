df7d4a784fd261f32d41864ab79e913b860a9171
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2026-02-06 16:45:34 +0000

    avoid circular imports when using pytest-xdist

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,6 +43,10 @@ def pytest_sessionstart(session):
     Called after the Session object has been created and
     before performing collection and entering the run test loop.
     """
+
+    # avoid circular imports when using xdist
+    import torch  # noqa: F401
+
     os.environ.setdefault("DTLOG_LEVEL", "error")
     os.environ.setdefault("DT_DEEPRT_VERBOSE", "-1")
 
